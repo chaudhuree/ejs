0ad576a034b944f0bb87509e322d165ead526d67
chaudhuree <chaudhuree00@gmail.com>
2022-12-19 21:52:41 +0600

    docs: use external css

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const ejs=require('ejs')
 
 const app = express()
 
+app.use(express.static("public"))
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('index',{pageTitle:"title changed"});
+  res.render('index',{pageTitle:"Home Page"});
+})
+app.get('/contact', (req, res) => {
+  res.render('contact',{pageTitle:"Contact Page",someText:"chaudhuree"});
 })
 
 
