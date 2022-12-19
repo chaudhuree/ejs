31af2d2239cad8386b686d80b41709f6c1ecfc30
chaudhuree <chaudhuree00@gmail.com>
2022-12-19 22:37:51 +0600

    done: functionality implemented with for and if else

--- index.js
+++ index.js
@@ -2,19 +2,32 @@ const express=require('express')
 const ejs=require('ejs')
 
 const app = express()
-
+// to use css and other files
 app.use(express.static("public"))
-
+// to get form data
+app.use(express.urlencoded({ extended:true}))
+// for ejs engine
 app.set('view engine', 'ejs')
 
+// constants
+let tasks=['eating','sleeping','walking','swimming']
+
+// home route
 app.get('/', (req, res) => {
-  res.render('index',{pageTitle:"Home Page"});
+  res.render('index',{pageTitle:"Home Page",tasks:tasks});
 })
+app.post('/', (req, res) => {
+  const {fname}=req.body
+  tasks.push(fname)
+  res.redirect("/");
+})
+
+// contact route
 app.get('/contact', (req, res) => {
   res.render('contact',{pageTitle:"Contact Page",someText:"chaudhuree"});
 })
 
 
 
-
+// server listen
 app.listen(5000,()=> console.log("app is running") )
